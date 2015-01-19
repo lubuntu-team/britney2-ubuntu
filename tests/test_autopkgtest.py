46281510e3d93c30b45e95d19689761355825760
Celso Providelo <celso.providelo@canonical.com>
2015-01-19 17:16:46 -0200

    Refactoring the existing test (autopkgtest), so its features can be re-used for other criterias (boottest) tests.

--- tests/test_autopkgtest.py
+++ tests/test_autopkgtest.py
@@ -6,133 +6,40 @@
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 
-import tempfile
-import shutil
+import apt_pkg
+import operator
 import os
 import sys
 import subprocess
 import unittest
-import apt_pkg
-import operator
 
-apt_pkg.init()
-architectures = ['amd64', 'arm64', 'armhf', 'i386', 'powerpc', 'ppc64el']
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, PROJECT_DIR)
 
-my_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from autopkgtest import ADT_EXCUSES_LABELS
+from tests import TestBase
 
 NOT_CONSIDERED = False
 VALID_CANDIDATE = True
 
-sys.path.insert(0, my_dir)
-from autopkgtest import ADT_EXCUSES_LABELS
+
+apt_pkg.init()
 
 
-class TestData:
-    def __init__(self):
-        '''Construct local test package indexes.
+class TestAutoPkgTest(TestBase):
 
-        The archive is initially empty. You can create new packages with
-        create_deb(). self.path contains the path of the archive, and
-        self.apt_source provides an apt source "deb" line.
-
-        It is kept in a temporary directory which gets removed when the Archive
-        object gets deleted.
-        '''
-        self.path = tempfile.mkdtemp(prefix='testarchive.')
-        self.apt_source = 'deb file://%s /' % self.path
-        self.series = 'series'
-        self.dirs = {False: os.path.join(self.path, 'data', self.series),
-                     True: os.path.join(self.path, 'data', '%s-proposed' % self.series)}
-        os.makedirs(self.dirs[False])
-        os.mkdir(self.dirs[True])
-        self.added_sources = {False: set(), True: set()}
-        self.added_binaries = {False: set(), True: set()}
-
-        # pre-create all files for all architectures
-        for arch in architectures:
-            for dir in self.dirs.values():
-                with open(os.path.join(dir, 'Packages_' + arch), 'w'):
-                    pass
-        for dir in self.dirs.values():
-            for fname in ['Dates', 'Blocks']:
-                with open(os.path.join(dir, fname), 'w'):
-                    pass
-            for dname in ['Hints']:
-                os.mkdir(os.path.join(dir, dname))
-
-        os.mkdir(os.path.join(self.path, 'output'))
-
-        # create temporary home dir for proposed-migration autopktest status
-        self.home = os.path.join(self.path, 'home')
-        os.environ['HOME'] = self.home
-        os.makedirs(os.path.join(self.home, 'proposed-migration',
-                                 'autopkgtest', 'work'))
-
-    def __del__(self):
-        shutil.rmtree(self.path)
-
-    def add(self, name, unstable, fields={}, add_src=True):
-        '''Add a binary package to the index file.
-
-        You need to specify at least the package name and in which list to put
-        it (unstable==True for unstable/proposed, or False for
-        testing/release). fields specifies all additional entries, e. g.
-        {'Depends': 'foo, bar', 'Conflicts: baz'}. There are defaults for most
-        fields.
-
-        Unless add_src is set to False, this will also automatically create a
-        source record, based on fields['Source'] and name.
-        '''
-        assert (name not in self.added_binaries[unstable])
-        self.added_binaries[unstable].add(name)
-
-        fields.setdefault('Architecture', architectures[0])
-        fields.setdefault('Version', '1')
-        fields.setdefault('Priority', 'optional')
-        fields.setdefault('Section', 'devel')
-        fields.setdefault('Description', 'test pkg')
-        if fields['Architecture'] == 'all':
-            for a in architectures:
-                self._append(name, unstable, 'Packages_' + a, fields)
-        else:
-            self._append(name, unstable, 'Packages_' + fields['Architecture'],
-                         fields)
-
-        if add_src:
-            src = fields.get('Source', name)
-            if src not in self.added_sources[unstable]:
-                self.add_src(src, unstable, {'Version': fields['Version'],
-                                             'Section': fields['Section']})
-
-    def add_src(self, name, unstable, fields={}):
-        '''Add a source package to the index file.
-
-        You need to specify at least the package name and in which list to put
-        it (unstable==True for unstable/proposed, or False for
-        testing/release). fields specifies all additional entries, which can be
-        Version (default: 1), Section (default: devel), and Extra-Source-Only.
-        '''
-        assert (name not in self.added_sources[unstable])
-        self.added_sources[unstable].add(name)
-
-        fields.setdefault('Version', '1')
-        fields.setdefault('Section', 'devel')
-        self._append(name, unstable, 'Sources', fields)
-
-    def _append(self, name, unstable, file_name, fields):
-        with open(os.path.join(self.dirs[unstable], file_name), 'a') as f:
-            f.write('''Package: %s
-Maintainer: Joe <joe@example.com>
-''' % name)
-
-            for k, v in fields.items():
-                f.write('%s: %s\n' % (k, v))
-            f.write('\n')
-
-
-class Test(unittest.TestCase):
     def setUp(self):
-        self.data = TestData()
+        super(TestAutoPkgTest, self).setUp()
+
+        # fake adt-britney script
+        self.adt_britney = os.path.join(
+            self.data.home, 'auto-package-testing', 'jenkins', 'adt-britney')
+        os.makedirs(os.path.dirname(self.adt_britney))
+
+        with open(self.adt_britney, 'w') as f:
+            f.write('''#!/bin/sh -e
+echo "$@" >> /%s/adt-britney.log ''' % self.data.path)
+        os.chmod(self.adt_britney, 0o755)
 
         # add a bunch of packages to testing to avoid repetition
         self.data.add('libc6', False)
@@ -145,24 +52,6 @@ class Test(unittest.TestCase):
         self.data.add('blue', False, {'Depends': 'libc6 (>= 0.9)',
                                       'Conflicts': 'green'})
         self.data.add('justdata', False, {'Architecture': 'all'})
-
-        self.britney = os.path.join(my_dir, 'britney.py')
-        self.britney_conf = os.path.join(my_dir, 'britney.conf')
-        assert os.path.exists(self.britney)
-        assert os.path.exists(self.britney_conf)
-
-        # fake adt-britney script
-        self.adt_britney = os.path.join(self.data.home, 'auto-package-testing',
-                                        'jenkins', 'adt-britney')
-        os.makedirs(os.path.dirname(self.adt_britney))
-
-        with open(self.adt_britney, 'w') as f:
-            f.write('''#!/bin/sh -e
-echo "$@" >> /%s/adt-britney.log ''' % self.data.path)
-        os.chmod(self.adt_britney, 0o755)
-
-    def tearDown(self):
-        del self.data
 
     def __merge_records(self, results, history=""):
         '''Merges a list of results with records in history.
@@ -235,28 +124,29 @@ args.func()
                            'rq': request,
                            'res': self.__merge_records(request, history)})
 
-    def run_britney(self, args=[]):
-        '''Run britney.
+    def do_test(self, unstable_add, adt_request, considered, expect=None,
+                no_expect=None, history=""):
+        for (pkg, fields) in unstable_add:
+            self.data.add(pkg, True, fields)
 
-        Assert that it succeeds and does not produce anything on stderr.
-        Return (excuses.html, britney_out).
-        '''
-        britney = subprocess.Popen([self.britney, '-v', '-c', self.britney_conf,
-                                    '--distribution=ubuntu',
-                                    '--series=%s' % self.data.series],
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE,
-                                   cwd=self.data.path,
-                                   universal_newlines=True)
-        (out, err) = britney.communicate()
-        self.assertEqual(britney.returncode, 0, out + err)
-        self.assertEqual(err, '')
+        self.make_adt_britney(adt_request, history)
 
-        with open(os.path.join(self.data.path, 'output', self.data.series,
-                               'excuses.html')) as f:
-            excuses = f.read()
+        (excuses, out) = self.run_britney()
+        #print('-------\nexcuses: %s\n-----' % excuses)
+        #print('-------\nout: %s\n-----' % out)
+        #print('run:\n%s -c %s\n' % (self.britney, self.britney_conf))
+        #subprocess.call(['bash', '-i'], cwd=self.data.path)
+        if considered:
+            self.assertIn('Valid candidate', excuses)
+        else:
+            self.assertIn('Not considered', excuses)
 
-        return (excuses, out)
+        if expect:
+            for re in expect:
+                self.assertRegexpMatches(excuses, re)
+        if no_expect:
+            for re in no_expect:
+                self.assertNotRegexpMatches(excuses, re)
 
     def test_no_request_for_uninstallable(self):
         '''Does not request a test for an uninstallable package'''
@@ -557,30 +447,6 @@ args.func()
             history="lightgreen 1 PASS lightgreen 1"
         )
 
-    def do_test(self, unstable_add, adt_request, considered, expect=None,
-                no_expect=None, history=""):
-        for (pkg, fields) in unstable_add:
-            self.data.add(pkg, True, fields)
-
-        self.make_adt_britney(adt_request, history)
-
-        (excuses, out) = self.run_britney()
-        #print('-------\nexcuses: %s\n-----' % excuses)
-        #print('-------\nout: %s\n-----' % out)
-        #print('run:\n%s -c %s\n' % (self.britney, self.britney_conf))
-        #subprocess.call(['bash', '-i'], cwd=self.data.path)
-        if considered:
-            self.assertIn('Valid candidate', excuses)
-        else:
-            self.assertIn('Not considered', excuses)
-
-        if expect:
-            for re in expect:
-                self.assertRegexpMatches(excuses, re)
-        if no_expect:
-            for re in no_expect:
-                self.assertNotRegexpMatches(excuses, re)
-
     def shell(self):
         # uninstallable unstable version
         self.data.add('yellow', True, {'Version': '1.1~beta',
@@ -593,4 +459,5 @@ args.func()
         subprocess.call(['bash', '-i'], cwd=self.data.path)
 
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
