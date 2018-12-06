b2e863fd6f6d7c7d50e9ce35259d637fb025b2f3
Iain Lane <iain.lane@canonical.com>
2018-12-06 09:35:39 +0000

    autopkgtest: Publish AMQP messages persistently
    We want to ask the broker to write them out to disk, hoepfully
    mitigating against it crashing.

--- britney2/policies/autopkgtest.py
+++ britney2/policies/autopkgtest.py
@@ -952,7 +952,9 @@ class AutopkgtestPolicy(BasePolicy):
         params = json.dumps(params)
 
         if self.amqp_channel:
-            self.amqp_channel.basic_publish(amqp.Message(src + '\n' + params), routing_key=qname)
+            self.amqp_channel.basic_publish(amqp.Message(src + '\n' + params,
+                                                         delivery_mode=2),  # persistent
+                                            routing_key=qname)
         else:
             assert self.amqp_file
             with open(self.amqp_file, 'a') as f:
