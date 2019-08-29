e245891d36ab7a33506cdd8fe3d42094641109f1
Ivo De Decker <ivodd@debian.org>
2019-08-29 22:05:35 +0000

    add reason for missing build

--- britney2/excusefinder.py
+++ britney2/excusefinder.py
@@ -509,6 +509,7 @@ class ExcuseFinder(object):
                 else:
                     excuse.policy_verdict = PolicyVerdict.REJECTED_CANNOT_DETERMINE_IF_PERMANENT
                     excuse.missing_build_on_arch(arch)
+                    excuse.addreason("missingbuild")
                     excuse.add_verdict_info(excuse.policy_verdict, text)
 
         # check if there is a `force' hint for this package, which allows it to go in even if it is not updateable
