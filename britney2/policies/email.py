fd20237673aac2a134246e2c22107069c6e16a13
Haw Loeung <haw.loeung@canonical.com>
2022-03-22 10:58:40 +1100

    Update email address used for emails
    @canonical.com is now DKIM signed and SPF published which means emails
    from proposed-migration running on snakefruit sending direct would
    likely be caught out. Since we're here, the project is Ubuntu related
    so switch to using an @ubuntu.com address instead.

--- britney2/policies/email.py
+++ britney2/policies/email.py
@@ -28,7 +28,7 @@ BOTS = {
     USER + "katie",
 }
 
-MESSAGE = """From: Ubuntu Release Team <noreply@canonical.com>
+MESSAGE = """From: Ubuntu Release Team <noreply+proposed-migration@ubuntu.com>
 To: {recipients}
 X-Proposed-Migration: notice
 Subject: [proposed-migration] {source_name} {version} stuck in {series}-proposed for {age} day{plural}.
@@ -287,7 +287,7 @@ class EmailPolicy(BasePolicy, Rest):
                         )
                     )
                     server = smtplib.SMTP(self.email_host)
-                    server.sendmail("noreply@canonical.com", emails, msg)
+                    server.sendmail("noreply+proposed-migration@ubuntu.com", emails, msg)
                     server.quit()
                     # record the age at which the mail should have been sent
                     last_sent = last_due
