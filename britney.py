acc93b376e941676af572df67530d8ff8503dc95
Steve Langasek <steve.langasek@canonical.com>
2024-01-08 12:51:38 -0800

    Revert "Temporarily disable autohinter due to a recursion problem"
    This reverts commit c93257d5cf8417cba3c72b6ed2404041753ab003.

--- britney.py
+++ britney.py
@@ -1464,9 +1464,6 @@ class Britney(object):
         """
         self.logger.info("> Processing hints from the auto hinter")
 
-        self.logger.info(">> Skipping auto hinting due to recursion problem")
-        return []
-
         sources_t = self.suite_info.target_suite.sources
         excuses = self.excuses
 
