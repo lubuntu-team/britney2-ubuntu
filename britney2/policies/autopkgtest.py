62aa68142d80fc4bfd07acce51b50969b19bb80a
Steve Langasek <steve.langasek@canonical.com>
2024-04-12 13:48:34 -0700

    fix syntax error

--- britney2/policies/autopkgtest.py
+++ britney2/policies/autopkgtest.py
@@ -182,7 +182,7 @@ class AutopkgtestPolicy(BasePolicy):
                             break
                         f_out.write(data)
                 content_length = f.getheader('content-length')
-                if http_code and content_length and os.path.getsize(new_file) != content_length):
+                if http_code and content_length and os.path.getsize(new_file) != content_length:
                     self.logger.info('Short read downloading autopkgtest results')
                     os.unlink(new_file)
                 else:
